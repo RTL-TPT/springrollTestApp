e6e52490d89ec9c6deee268f68fbfe8d9585add1
Dennis Ramirez <dennispr@gmail.com>
2018-02-14 12:57:42 -0600

    Woring Pixi.js build

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# SpringrollTestApp (*SpringrollTestApp*)
+# pixispring (*pixispring*)
 
 This project was started using [SpringRoll Studio](https://github.com/springroll/SpringRollStudio) with template Default (1.5.0).
--- src/main.js
+++ src/main.js
@@ -2,25 +2,72 @@
 
 	// Library depencencies
 	var Application = include('springroll.Application'),
-		Display = include('springroll.easeljs.EaselJSDisplay');
+		Texture = include('PIXI.Texture'),
+		PixiDisplay = include('springroll.pixi.PixiDisplay'),
+		PixiButton = include('springroll.pixi.Button');
 
 	// Create a new application
 	var app = new Application({
-		name: "SpringrollTestApp",
-		canvasId: "stage",
-		display: Display,
-		displayOptions:	{
-			clearView: true,
-		}
+		name: "pixispring",
+		debug: true,
+		preload: [
+			{
+				id: "PixiAssets",
+				image: 'assets/images/button.png',
+				atlas: 'assets/images/button.json',
+				type: 'pixi'
+			},
+			{
+				id: 'Button',
+				src: 'assets/images/button.png'
+			}
+		]
 	});
 
 	// Handle when app is ready to use
 	app.on('init', function()
 	{
-		// Start application
+		this.initPixi();
 	});
+	
+	
+	app.initPixi = function()
+	{
+		var display = this.addDisplay("stage", PixiDisplay, {
+			clearView: true,
+			width: 1047,
+			backgroundColor: 0xccddff
+		});
+	
+		// Setup the toggle button
+		var texture = this.getCache('PixiAssets');
+		var button = new PixiButton(
+			{
+				up : texture.getFrame('button_up'),
+				over : texture.getFrame('button_over'),
+				down : texture.getFrame('button_down'),
+				disabled : texture.getFrame('button_disabled')
+			},
+			{
+				text : 'button',
+				style : {
+					font : '20px Arial',
+					fill : "#ffffff"
+				}
+			}
+		);
 
-	// Assign to the window for easy access
-	window.app = app;
+		button.position.x = (display.width - button.width) / 2;
+		button.position.y = (display.height - button.height) / 2;
+		button.on('buttonPress', this.toggleDisplay.bind(this, false));
+
+		display.stage.addChild(button);
+	};
+	
+	app.toggleDisplay = function(showPixi)
+	{
+		pixi.enabled = showPixi;
+		pixi.visible = showPixi;
+	};
 	
 }());
