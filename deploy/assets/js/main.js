7969626a268f96a261da1e07e66a64ad05e8168f
Dennis Ramirez <dennispr@gmail.com>
2018-02-14 13:50:17 -0600

    adding .bowerrc file to direct components to components dir

--- deploy/assets/js/main.js
+++ deploy/assets/js/main.js
@@ -1,1 +1,74 @@
-!function(){var a=include("springroll.Application"),b=include("springroll.pixi.PixiDisplay"),c=include("springroll.pixi.Button"),d=new a({name:"pixispring",debug:!0,preload:[{id:"PixiAssets",image:"assets/images/button.png",atlas:"assets/images/button.json",type:"pixi"},{id:"Button",src:"assets/images/button.png"}]});d.on("init",function(){this.initPixi()}),d.initPixi=function(){var a=this.addDisplay("stage",b,{clearView:!0,width:1047,backgroundColor:13426175}),d=this.getCache("PixiAssets"),e=new c({up:d.getFrame("button_up"),over:d.getFrame("button_over"),down:d.getFrame("button_down"),disabled:d.getFrame("button_disabled")},{text:"button",style:{font:"20px Arial",fill:"#ffffff"}});e.position.x=(a.width-e.width)/2,e.position.y=(a.height-e.height)/2,e.on("buttonPress",this.toggleDisplay.bind(this,!1)),a.stage.addChild(e)},d.toggleDisplay=function(a){pixi.enabled=a,pixi.visible=a}}();
+(function(){
+
+	// Library depencencies
+	var Application = include('springroll.Application'),
+		Texture = include('PIXI.Texture'),
+		PixiDisplay = include('springroll.pixi.PixiDisplay'),
+		PixiButton = include('springroll.pixi.Button');
+
+	// Create a new application
+	var app = new Application({
+		name: "pixispring",
+		debug: true,
+		preload: [
+			{
+				id: "PixiAssets",
+				image: 'assets/images/button.png',
+				atlas: 'assets/images/button.json',
+				type: 'pixi'
+			},
+			{
+				id: 'Button',
+				src: 'assets/images/button.png'
+			}
+		]
+	});
+
+	// Handle when app is ready to use
+	app.on('init', function()
+	{
+		this.initPixi();
+	});
+	
+	
+	app.initPixi = function()
+	{
+		var display = this.addDisplay("stage", PixiDisplay, {
+			clearView: true,
+			width: 1047,
+			backgroundColor: 0xccddff
+		});
+	
+		// Setup the toggle button
+		var texture = this.getCache('PixiAssets');
+		var button = new PixiButton(
+			{
+				up : texture.getFrame('button_up'),
+				over : texture.getFrame('button_over'),
+				down : texture.getFrame('button_down'),
+				disabled : texture.getFrame('button_disabled')
+			},
+			{
+				text : 'button',
+				style : {
+					font : '20px Arial',
+					fill : "#ffffff"
+				}
+			}
+		);
+
+		button.position.x = (display.width - button.width) / 2;
+		button.position.y = (display.height - button.height) / 2;
+		button.on('buttonPress', this.toggleDisplay.bind(this, false));
+
+		display.stage.addChild(button);
+	};
+	
+	app.toggleDisplay = function(showPixi)
+	{
+		pixi.enabled = showPixi;
+		pixi.visible = showPixi;
+	};
+	
+}());
+//# sourceMappingURL=main.js.map
